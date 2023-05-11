8c65fa8ab13f6088bde3ffa30bd5a9b62688fde2
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-05-11 17:09:15 +0900

    [unrated] Title: 과일 장수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,18 +1,38 @@
 function solution(k, m, score) {
     let answer = [] ; 
-    let result = 0;
-    let apple = score.sort((a,b)=>b-a);
+    let apple = score.sort();
     for(let i = 0; i < apple.length; i=i+m){
         answer.push(apple.slice(i,i+m))
     }
-    answer.map((e,index)=>{
-        e.length===m ? result += (e[m-1] *e.length)  : 0
+    answer.map((e)=>{
+        console.log(Math.min(e))
     })
-    return result;
+    console.log(Math.min(answer))
+    return answer;
 }
 //사과의 최대 점수 k
 //한 상자에 들어가는 사과의 수  m 
 //사과들의 점수 score
 //최저점수 * 한상자 담긴 사과 개수 * 상자의 개수 
-//남은 상자는 버린다.
 
+
+
+
+
+
+
+
+// function solution(k, m, score) {
+//     var answer = 0;
+//    score.sort((a, b) => b - a); //1.
+//     let arr=[];
+//   for(let i=0; i<score.length; i+=m) {
+//       arr.push(score.slice(i, i+m)); //2.
+//   }
+//     for(let j=0; j<arr.length; j++){// 3.
+//         if(arr[j].length ===m){ //4.
+//            answer+= arr[j][m-1]*m; //5.
+//         }
+//     }
+//     return answer;
+// }

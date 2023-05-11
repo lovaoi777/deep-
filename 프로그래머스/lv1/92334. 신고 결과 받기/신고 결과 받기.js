7d04da62f40b20f523b4b27ce8bd3cd1a90305c1
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-05-11 18:43:01 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,27 +1,34 @@
 function solution(id_list, report, k) {
-    const answer = new Array(id_list.length);
-    answer.fill(0) 
-    const report_list = {} //
-
-
-    id_list.map((user)=>{
-        report_list[user] = [] //key로 userid를 value로 빈 배열을 가지는 객체
+    var answer = [];
+    var User = {}
+    var EUser = {} //정지 유저 
+    var result = [];
+ //객체 생성
+    id_list.map((e)=>{
+        User[e] = []
+        EUser[e] = 0
     })
-
-    report.map((user)=>{
-        const [user_id, report_id] = user.split(' ')
-        if(!report_list[report_id].includes(user_id)){
-            report_list[report_id].push(user_id)
-        }
-                    
+    //객체 배열 구조 분해
+        report.map((e)=>{
+            let [a,b] = e.split(' ')
+            if(User[a].includes(b)==false){
+            User[a].push(b) 
+            EUser[b] += 1
+                }
+        })
+    //정지 유저 찾기
+    id_list.map((e)=>{
+        if(EUser[e] >= k)
+            answer.push(e)
     })
-
-    for(const key in report_list){
-        if(report_list[key].length >= k){ //이용정지 유저
-            report_list[key].map((user)=>{
-                answer[id_list.indexOf(user)] += 1
-            })
-        }
-    }
-    return answer;
+    id_list.map((user)=> { 
+        var sum =0
+        answer.map((e)=>{
+            if(User[user].includes(e))
+              sum+=1
+        })
+        result.push(sum)
+    })
+    // console.log(EUser)
+    return  result;
 }

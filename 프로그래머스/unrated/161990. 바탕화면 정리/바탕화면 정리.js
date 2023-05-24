e8f0acf2dcc70e4c0f9536e53c8d7584edf5953d
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-05-25 02:02:01 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,27 @@
+function solution(wallpaper) {
+    var answer = [];
+    var min = [];
+    wallpaper.map((e)=>{
+        e=e.split('')
+        answer.push(e)
+    })
+    answer.map((e,index1)=>{
+        e.map((shap,index2)=>{
+             if(shap=='#'){
+                min.push([index1,index2,index1+1,index2+1])
+             }
+        })
+    })
+    let result = [min[0][0]]
+    let a = [min[0][1]]
+    let b = [min[0][2]]
+    let c = [min[0][3]]
+    min.map((e)=>{
+        a>e[1]?a=e[1]:''
+        b<e[2]?b=e[2]:''
+        c<e[3]?c=e[3]:''
+    })
+    result.push(a,b,c)
+    let newArray2 = [].concat(...result);
+    return newArray2;
+}

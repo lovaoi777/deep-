47e2a6a6b68950ba4c3ca9224663799740f97fd6
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-07-05 18:00:45 +0900

    [level 2] Title: 하노이의 탑, Time: 5.13 ms, Memory: 48.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,19 @@
+
+
+
+function solution(n) {
+    var answer = [];
+    function hanoi(n , from , start, end){
+    if(n===1) answer.push([from,end])
+        else{
+            hanoi(n-1, from , end,start)
+            answer.push([from,end])
+            hanoi(n-1, start , from , end)
+        }
+}
+    hanoi(n,1,2,3)
+    return answer;
+}
+//하노이탑
+//1. 한번에 하나의 원판만 옯길  수 있다.
+//2. 큰 원판이 작은 원판 위에 있어서는 안된다 . 큰 > 작

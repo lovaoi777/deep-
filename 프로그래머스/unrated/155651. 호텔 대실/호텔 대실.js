048c893887f321864b5f8aec73c78f39df59bb6d
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-08-10 18:51:37 +0900

    [unrated] Title: 호텔 대실, Time: 14.75 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,24 @@
+function solution(book_time) {
+    var answer = 0;
+    let room = [] ;
+    book_time = book_time.sort()
+    //문자 ->  분으로 변환
+    book_time.map((e)=> { 
+    let [startTime , finishTime] = e;
+    let [start_hour , start_min] = startTime.split(':');
+    let [finish_hour , fnish_min] = finishTime.split(':');
+    let stime = Number(start_hour * 60) + Number(start_min);
+    let ftime = Number(finish_hour * 60) + Number(fnish_min)+10;   
+
+    const idx = room.findIndex(x => x <= stime);
+    if(idx === -1) {
+            room.push(ftime)
+        } else {
+            room[idx] = ftime;
+        }
+    }
+                 )
+    console.log(room)
+           return room.length
+        
+}

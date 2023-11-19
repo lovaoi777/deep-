00e37d9c69adb16bc241a58112a998fa0feb15c3
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-11-19 23:38:14 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,14 @@
+function solution(number) {
+    var answer = 0;
+    let result = []; 
+    for(let i= 0; i<number.length; i++ ){
+        for(let j= i+1;j<number.length; j++){
+            for(let z =j+1;z<number.length;z++){
+                if(number[i]+number[j]+number[z]===0){
+                    answer+=1
+                }
+            }
+        }
+    }
+    return answer;
+}

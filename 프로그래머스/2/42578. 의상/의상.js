9c999d45a351db3d79beb20ccd074fd1369d6c12
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-11-28 16:03:24 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,13 @@
 function solution(clothes) {
     const clothesMap = {};
     let answer = 1;
-    // 의상 종류별로 갯수를 저장
-    clothes.forEach(arr => {    //clothes 배열을 순회하면서 각 요소를 arr배열에 할당하고 코드 블록을 실행. arr은 clothes 배열의 각 요소를 나타내는 임시 변수
+    clothes.forEach(arr => {    
         const [type, name] = arr;
-        if(clothesMap.hasOwnProperty(name)) {	//clothesMap 객체에 name이라는 속성이 있는지 확인하는 조건문
-            clothesMap[name]++;    	//이미 존재한다면 해당 종류의 이상 개수를 1증가
+        if(clothesMap.hasOwnProperty(name)) {	
+            clothesMap[name]++;    	
         }
         else {
-            clothesMap[name] = 1;  //객체에 해당 종류의 의상이 존재하지 않는 경우, 해당 의상이 처음으로 추가되는 것이므로 name 속성을 추가하고 1로 초기화
+            clothesMap[name] = 1;  
         }
     })
 

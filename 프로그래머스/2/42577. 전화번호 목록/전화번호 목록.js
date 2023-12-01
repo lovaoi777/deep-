0616ac7bab9eb90b91dc427c7a357bd9da27398e
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-12-01 18:42:29 +0900

    [level 2] Title: 전화번호 목록, Time: 99.19 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,21 +1,5 @@
-// function solution(phone_book) {
-//     var answer = true;
-//     var count = [];
-//     phone_book=phone_book.sort((a,b)=>b-a)
-//     for(let i =0 ; i<phone_book.length;i++){
-//         console.log(count.includes(phone_book[i]),count,phone_book[i])
-//         if(count.includes(phone_book[i])==true){
-//             return false;
-//         }
-//         else{
-//             count.push(phone_book[i])
-//         }
-//     }
-//     // console.log(count.includes(count[2]))
-//     return answer;
-// }
-function solution(phone_book) {
-    let answer = phone_book.sort().some((value, index, self) => self[index + 1]?.indexOf(value) === 0);
-
+function solution(phoneBook) {
+    var answer = true;
+    answer =phoneBook.sort().some((value,index,arr)=>arr[index+1]?.indexOf(value)==0)
     return !answer;
 }

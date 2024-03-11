7a90e4735807f70e97ea1f93469cf61129c2679b
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2024-03-12 05:12:31 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,12 +1,14 @@
 function solution(n) {
     var answer = '';
     for(let i=1;i<=n;i++){
-        if(i%2==1){
-            answer+='수';
-        }
-        else if(i%2==0){
-            answer+='박';
-        }
+        i%2==1? answer+='수' :  answer+='박';
     }
     return answer;
 }
+
+//2 -> 수박
+//3 -> 수박수
+//4 -> 수박수박 
+//5 -> 수박수박수
+
+

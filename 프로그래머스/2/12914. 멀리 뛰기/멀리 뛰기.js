27e1065062a073b70222f5d24225bd4e2dabf053
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2024-04-15 11:06:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,20 +1,22 @@
 // function solution(n) {
-//     const count = 0;
-//     const result = [0,1]
-//     for (let i = 2; i <= n; i++) { 
-//         result.push((result[i-1] + result[i-2]) % 1234567);
+//     var count = 0;
+//     var result = []
+//     result[1]=1
+//     result[2]=2
+//     for (let i = 3; i <= n; i++) { 
+//         result.push(result[i-1] + result[i-2] %1234567);
 //     } 
-//     count= result[n]
-//     return n% 1234567;
+//     count=result[n];
+//     return count%1234567;
 // }
-// return n == 1 ? 1 : b;
-function solution(n) {
+function solution(n){
     var answer = 0;
-    var dp=[];
-    dp[1]=1;
-    dp[2]=2;
-    for(var i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2] %1234567;
+    var result = [];
+    result[1]=1;
+    result[2]=2;
+    for(let i =3;i<= n ; i++){
+        result.push(result[i-1]+ result[i-2]% 1234567)
     }
-    answer=dp[n];
-    return answer%1234567;}
+    answer = result[n]
+    return answer% 1234567;
+}
